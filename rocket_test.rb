f70c77b42885b1d3226ac8b7a4d9cadafcf8e97b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-31 22:58:47 -0400

    test for lift off complete

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,41 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def teardown
+    @rocket = nil
+  end
+
+  def test_lift_off_true
+    # arrange
+    result = @rocket.lift_off
+    expected = true
+    # act
+    assert_equal(expected, result)
+    # assert
+  end
+
+  def test_lift_off_false
+    @rocket = Rocket.new({ :flying => true })
+    result = @rocket.lift_off
+    expected = false
+
+    assert_equal(expected, result)
+  end
+
+  def test_land_true
+    @rocket.lift_off
+    result = @rocket.land
+    expected = true
+    assert_equal(expected, result)
+
+  end
+
+  def test_land_false
+    #code
+  end
+
 end
